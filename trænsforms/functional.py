9de4f1a0353b8ee82186dabed18a8490be6dabe1
Holim Lim <holim0711@outlook.com>
2021-04-23 19:56:31 +0900

    add Gaussian Blur

--- trænsforms/functional.py
+++ trænsforms/functional.py
@@ -1,7 +1,11 @@
+import torch
+import numpy as np
 from PIL import Image
+import cv2
 
 __all__ = [
     'square_resize',
+    'gaussian_blur',
 ]
 
 
@@ -20,3 +24,9 @@ def square_resize(img, size, interpolation=Image.BILINEAR, background_color=0):
     new.paste(img, ((size - x) // 2, (size - y) // 2))
 
     return new
+
+
+def gaussian_blur(img, kernel_size, σ=(0.1, 2.0)):
+    img = np.array(img)
+    σ = σ[0] + (σ[1] - σ[0]) * torch.rand(1).item()
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), σ)
--- trænsforms/transforms.py
+++ trænsforms/transforms.py
@@ -3,11 +3,11 @@ from .functional import *
 
 __all__ = [
     'SquareResize',
+    'GaussianBlur',
 ]
 
 
 class SquareResize():
-
     def __init__(self, size, interpolation=Image.BILINEAR, background_color=0):
         self.size = int(size)
         self.interpolation = interpolation
@@ -15,3 +15,12 @@ class SquareResize():
 
     def __call__(self, img):
         return square_resize(img, self.size, self.interpolation, self.bg_color)
+
+
+class GaussianBlur():
+    def __init__(self, kernel_size, σ=(0.1, 2.0)):
+        self.kernel_size = kernel_size
+        self.σ = σ
+
+    def __call__(self, img):
+        return gaussian_blur(img, self.kernel_size, self.σ)
